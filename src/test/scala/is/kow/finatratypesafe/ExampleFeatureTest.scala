9bf0c987247b9916290e596f6d78d676f0621baa
David Kowis <david_kowis@homedepot.com>
2015-09-03 11:04:14 -0500

    Here's a proper failing test
    This demonstrates that it's using the right values from the classpath
    application.conf, but it's not using anything from the passed in path to
    the config file.

--- src/test/scala/is/kow/finatratypesafe/ExampleFeatureTest.scala
+++ src/test/scala/is/kow/finatratypesafe/ExampleFeatureTest.scala
@@ -2,6 +2,7 @@ package is.kow.finatratypesafe
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
+import java.util.Base64
 
 import com.twitter.finagle.http.Status._
 import com.twitter.finatra.http.test.{EmbeddedHttpServer, HttpTest}
@@ -33,6 +34,9 @@ class ExampleFeatureTest extends HttpTest {
     )
   )
 
+  val authData = s"Basic ${new String(Base64.getEncoder.encode("requiredUser:requiredPass".getBytes(StandardCharsets.UTF_8)))}"
+
+
   "It should be requiring authentication by default" in {
     server.httpGet(
       path = "/examples",
@@ -44,6 +48,7 @@ class ExampleFeatureTest extends HttpTest {
     server.httpGet(
       path = "/examples",
       andExpect = Ok,
+      headers = Map("Authorization" -> authData),
       withJsonBody =
         """
           |[ "test1", "test2", "4321"]
