c573e5e83055a99fb4952b84fd5e1b7a822ff8e9
David Kowis <david_kowis@homedepot.com>
2015-09-03 11:07:14 -0500

    Logging and notes

--- src/main/scala/is/kow/finatratypesafe/modules/FinatraTypesafeConfigModule.scala
+++ src/main/scala/is/kow/finatratypesafe/modules/FinatraTypesafeConfigModule.scala
@@ -1,17 +1,21 @@
 package is.kow.finatratypesafe.modules
 
 import com.github.racc.tscg.TypesafeConfigModule
-import com.twitter.inject.TwitterModule
+import com.twitter.inject.{Logging, TwitterModule}
 import com.typesafe.config.{Config, ConfigFactory}
 
-object FinatraTypesafeConfigModule extends TwitterModule {
+object FinatraTypesafeConfigModule extends TwitterModule with Logging {
   val configurationFile = flag("config.file", "", "Optional config file to override settings")
 
+  //TODO: this is still reading the flag too soon, so it'll always be ""
   lazy val config: Config = {
     val specified = configurationFile()
+    logger.info(s"FLAG VALUE: $specified")
     if (specified.nonEmpty) {
+      logger.info(s"LOADING CONFIG FILE: $specified")
       ConfigFactory.load(specified)
     } else {
+      logger.warn("LOADING DEFAULT CONFIGURATION ONLY!")
       ConfigFactory.load()
     }
   }
